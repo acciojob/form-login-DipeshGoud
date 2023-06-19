0cf435e54a8cb8d8d08ba35e665dd101382d23c3
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-06-19 15:37:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
     //Write your code here
-	let firstName = document.getElementsByName("fname")[0];
-	let lastName = document.getElementsByName("lname")[0];
+	let firstName = document.getElementsByName("fname")[0].value;
+	let lastName = document.getElementsByName("lname")[0].value;
 
 	alert(firstName+" "+lastName);
 }
